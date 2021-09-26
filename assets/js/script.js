459be2d34813f546b08bf82448562990f96eb5f8
lexach91 <konovalov1991@mail.ru>
2021-09-26 14:39:52 +0800

    Change dataset, add geojson, and add code to display choropleth map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,51 @@
-const parseDate = d3.timeParse("%m/%d/%Y")
-d3.csv("assets/data/covid_19_data.csv")
-    .catch((err) => {throw err})
-    .then(data => {
-        data.forEach(d => {
-            d.Confirmed = +d.Confirmed;
-            d.Recovered = +d.Recovered;
-            d.Deaths = +d.Deaths;
-            d.ObservationDate = parseDate(d.ObservationDate);
-        })
+const parseDate = d3.timeParse("%Y-%m-%d");
+// 2020-2-15
+const mapChart = dc.geoChoroplethChart("#map");
+d3.csv("assets/data/worldometer_coronavirus_daily_data.csv")
+  .catch((err) => {
+    throw err;
+  })
+  .then((data) => {
+    data.forEach((d) => {
+      d.cumulative_total_cases = +d.cumulative_total_cases;
+      d.daily_new_cases = +d.daily_new_cases;
+      d.active_cases = +d.active_cases;
+      d.cumulative_total_deaths = +d.cumulative_total_deaths;
+      d.daily_new_deaths = +d.daily_new_deaths;
+      d.date = parseDate(d.date);
+    });
+    d3.json("assets/data/countries.geojson").then((worldMap) => {
+      // console.log(worldMap)
 
-        const cData = crossfilter(data);
-        const all = cData.groupAll();
-    })
+      const cData = crossfilter(data);
+      const all = cData.groupAll();
+
+      const dateDim = cData.dimension(dc.pluck("date"));
+      const countriesDim = cData.dimension(dc.pluck("country"));
+
+      const casesByDayGroup = dateDim
+        .group()
+        .reduceSum(dc.pluck("cumulative_total_cases"));
+
+      const casesByCountryGroup = countriesDim
+        .group()
+        .reduceSum(dc.pluck("cumulative_total_cases"));
+
+      mapChart
+        .width(990)
+        .height(500)
+        .dimension(countriesDim)
+        .group(casesByCountryGroup)
+        .projection(d3.geoMercator())
+        .overlayGeoJson(worldMap.features, "country", function (d) {
+        //   console.log(d.properties.name);
+          return d.properties.ADMIN;
+        });
+      // .overlayGeoJson(worldMap, "Countries", function (d) {
+      //   return d.properties["Country/Region"];
+      // })
+      // .projection(d3.geoMercatorRaw);
+
+      dc.renderAll();
+    });
+  });
